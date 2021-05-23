61cf4f20f461d702bfd87c58a7379d4e66490e8f
ziga_miklosic <ziga.miklosic@gmail.com>
2021-05-23 09:04:26 +0200

    - implementation of parameter logic

--- src/par.h
+++ src/par.h
@@ -21,6 +21,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include <stdint.h>
 #include <stdbool.h>
+
+#include "project_config.h"
 #include "../../par_cfg.h"
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -39,14 +41,72 @@ typedef enum
 	ePAR_ERROR_NVM			= 0x08,		/**<Parameter storage to NMV error */
 }par_status_t;
 
+/**
+ * 	Parameters type enumeration
+ */
+typedef enum
+{
+	ePAR_TYPE_U8 = 0,	/**<Unsigned 8-bit value */
+	ePAR_TYPE_U16,		/**<Unsigned 16-bit value */
+	ePAR_TYPE_U32,		/**<Unsigned 32-bit value */
+	ePAR_TYPE_I8,		/**<Signed 8-bit value */
+	ePAR_TYPE_I16,		/**<Signed 16-bit value */
+	ePAR_TYPE_I32,		/**<Signed 32-bit value */
+	ePAR_TYPE_F32,		/**<32-bit floating value */
+	ePAR_TYPE_NUM_OF
+}par_type_list_t;
+
+/**
+ * 	Parameter R/W access
+ */
+typedef enum
+{
+	ePAR_ACCESS_RO = 0,			/**<Parameter read only */
+	ePAR_ACCESS_RW				/**<Parameter read/write */
+}par_io_acess_t;
+
+/**
+ * 	Supported data types
+ */
+typedef union
+{
+	uint8_t			u8;			/**<Unsigned 8-bit value */
+	uint16_t		u16;		/**<Unsigned 16-bit value */
+	uint32_t		u32;		/**<Unsigned 32-bit value */
+	int8_t			i8;			/**<Signed 8-bit value */
+	int16_t			i16;		/**<Signed 16-bit value */
+	int32_t			i32;		/**<Signed 32-bit value */
+	float32_t		f32;		/**<32-bit floating value */
+} par_type_t;
+
+/**
+ * 	Parameter data settings
+ *
+ * @note	Single parameter object has size of 28 bytes on
+ * 			arm-gcc compiler.
+ */
+typedef struct
+{
+ 	par_type_t			min;			/**<Minimum value of parameter */
+	par_type_t			max;			/**<Maximum value of parameter */
+	par_type_t			def;			/**<Default value of parameter */
+	char*			name;			/**<Name of variable */
+	char*			unit;			/**<Unit of parameter */
+	uint16_t		id;				/**<Variable ID */
+	par_type_list_t		type;			/**<Parameter type */
+	par_io_acess_t 		access;			/**<Parameter access from external device point-of-view */
+ 	bool			persistant;		/**<Parameter persistance flag */
+} par_cfg_t;
+
 ////////////////////////////////////////////////////////////////////////////////
 // Functions Prototypes
 ////////////////////////////////////////////////////////////////////////////////
 par_status_t 	par_init			(void);
 const bool		par_is_init			(void);
-par_status_t 	par_set				(const par_id_t id, const void * p_val);
-par_status_t 	par_get				(const par_id_t id, void * const p_val);
+par_status_t 	par_set				(const par_name_t par, const void * p_val);
+par_status_t 	par_get				(const par_name_t par, void * const p_val);
 par_status_t 	par_set_to_default	(void);
+par_status_t 	par_get_config		(const par_name_t par_name, par_cfg_t * const p_par_cfg);
 
 #endif // _PAR_H_
 
