0c821d4de37f919fdb394927a9af1b0a60839abd
ziga_miklosic <ziga.miklosic@gmail.com>
2021-05-23 10:31:43 +0200

    - implementation of RAM storage reservation

--- src/par.c
+++ src/par.c
@@ -32,6 +32,8 @@
 #include "par.h"
 #include "../../par_cfg.h"
 
+#include <stdlib.h>
+
 ////////////////////////////////////////////////////////////////////////////////
 // Definitions
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,17 +49,41 @@
 /**
  * 	Pointer to parameter table
  */
-static const par_cfg_t * p_par_table = NULL;
+static const par_cfg_t * gp_par_table = NULL;
 
 /**
  * 	Initialization guard
  */
 static bool gb_is_init = false;
 
+/**
+ * 	Parameter active value that is stored in RAM and its
+ * 	address offsets
+ */
+static uint8_t * 	gpu8_par_value 						= NULL;
+static uint32_t 	gu32_par_addr_offset[ ePAR_NUM_OF ] = { 0 };
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Function Prototypes
 ////////////////////////////////////////////////////////////////////////////////
+static par_status_t par_allocate_ram_space	(uint8_t * p_ram_space);
+static uint32_t 	par_calc_ram_usage		(void);
+static uint8_t 		par_get_data_type_size	(const par_type_list_t par_type);
+
+
+static par_status_t par_set_check_validy(const par_name_t par_name);
+
+static par_status_t par_set_u8	(const par_name_t par_name, uint8_t u8_val);
+static par_status_t par_set_i8	(const par_name_t par_name, int8_t i8_val);
+
+
+/*static par_status_t par_set_u16	(const par_name_t par_name, uint16_t u16_val);
+static par_status_t par_set_i16	(const par_name_t par_name, int16_t i16_val);
+static par_status_t par_set_u32	(const par_name_t par_name, uint32_t u32_val);
+static par_status_t par_set_i32	(const par_name_t par_name, int32_t i32_val);
+static par_status_t par_set_f32	(const par_name_t par_name, float32_t f32_val);*/
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -70,11 +96,24 @@ par_status_t par_init(void)
 	par_status_t status = ePAR_OK;
 
 	// Get parameter table
-	p_par_table = par_cfg_get_table();
+	gp_par_table = par_cfg_get_table();
 
-	PAR_ASSERT( NULL != p_par_table );
+	PAR_ASSERT( NULL != gp_par_table );
 
 	gb_is_init = true;
+
+	// Check if defined correctly
+	// TODO: unique par ID ?
+	// TODO: min lower than max ?
+	// TODO: def within min & max value ?
+
+
+	// Allocate space in RAM
+	par_allocate_ram_space( gpu8_par_value );
+
+
+
+
 
 	return status;
 }
@@ -92,10 +131,406 @@ par_status_t par_get_config(const par_name_t par_name, par_cfg_t * const p_par_c
 	PAR_ASSERT( NULL != p_par_cfg );
 	PAR_ASSERT( par_name < ePAR_NUM_OF );
 
-	*p_par_cfg = p_par_table[ par_name ];
+	*p_par_cfg = gp_par_table[ par_name ];
+
+	par_allocate_ram_space( gpu8_par_value );
 
 	return status;
 }
+
+
+par_status_t par_set(const par_name_t par_name, const void * p_val)
+{
+	par_status_t status;
+
+	switch ( gp_par_table[ par_name ].type )
+	{
+		case ePAR_TYPE_U8:
+			status = par_set_u8( par_name, *(uint8_t*) p_val );
+			break;
+
+		case ePAR_TYPE_I8:
+			status = par_set_i8( par_name, *(int8_t*) p_val );
+			break;
+
+/*		case ePAR_TYPE_U16:
+			status = par_set_u16( par_name, *(uint16_t*) p_val );
+			break;
+
+		case ePAR_TYPE_I16:
+			status = par_set_i16( par_name, *(int16_t*) p_val );
+			break;
+
+		case ePAR_TYPE_U32:
+			status = par_set_u32( par_name, *(uint32_t*) p_val );
+			break;
+
+		case ePAR_TYPE_I32:
+			status = par_set_i32( par_name, *(int32_t*) p_val );
+			break;
+
+		case ePAR_TYPE_F32:
+			status = par_set_f32( par_name, *(float32_t*) p_val );
+			break;*/
+
+		default:
+			PAR_ASSERT( 0 );
+			break;
+	}
+
+	return status;
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+* @} <!-- END GROUP -->
+*/
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+/**
+*@addtogroup KERNEL_FUNCTIONS
+* @{ <!-- BEGIN GROUP -->
+*
+* 	Kernel functions of device parameters
+*/
+////////////////////////////////////////////////////////////////////////////////
+
+
+static par_status_t par_allocate_ram_space(uint8_t * p_ram_space)
+{
+	par_status_t 	status 		= ePAR_OK;
+	uint32_t		ram_size	= 0UL;
+
+	// Calculate total size of RAM
+	ram_size = par_calc_ram_usage();
+
+	// Allocate space in RAM
+	p_ram_space = malloc( ram_size );
+	PAR_ASSERT( NULL != p_ram_space );
+
+	return status;
+}
+
+
+static uint32_t par_calc_ram_usage(void)
+{
+	par_cfg_t 		par_cfg 	= { 0 };
+	uint32_t 		i 			= 0UL;
+	uint32_t		total_size	= 0UL;
+
+	// For every parameter
+	for ( i = 0; i < ePAR_NUM_OF; i++ )
+	{
+		// Get parameter configs
+		par_get_config( i, &par_cfg );
+
+        // Align addresses
+        if	(	( par_cfg.type == ePAR_TYPE_U16 )
+        	|| 	( par_cfg.type == ePAR_TYPE_I16 ))
+        {
+            while(( total_size % 2 ) != 0 )
+            {
+            	total_size++;
+            }
+        }
+        else if (	( par_cfg.type == ePAR_TYPE_U32 )
+        		|| 	( par_cfg.type == ePAR_TYPE_I32 )
+				|| 	( par_cfg.type == ePAR_TYPE_F32 ))
+        {
+            while(( total_size % 4 ) != 0 )
+            {
+            	total_size++;
+            }
+        }
+        else
+        {
+        	// No actions...
+        }
+
+        // Store par RAM address offset
+        gu32_par_addr_offset[i] = total_size;
+
+        // Accumulate total RAM space
+        total_size += par_get_data_type_size( par_cfg.type );
+	}
+
+	return total_size;
+}
+
+
+static uint8_t par_get_data_type_size(const par_type_list_t par_type)
+{
+	uint8_t type_size = 0U;
+
+	switch ( par_type )
+	{
+		case ePAR_TYPE_U8:
+			type_size = sizeof( uint8_t );
+			break;
+
+		case ePAR_TYPE_I8:
+			type_size = sizeof( int8_t );
+			break;
+
+		case ePAR_TYPE_U16:
+			type_size = sizeof( uint16_t );
+			break;
+
+		case ePAR_TYPE_I16:
+			type_size = sizeof( int16_t );
+			break;
+
+		case ePAR_TYPE_U32:
+			type_size = sizeof( uint32_t );
+			break;
+
+		case ePAR_TYPE_I32:
+			type_size = sizeof( int32_t );
+			break;
+
+		case ePAR_TYPE_F32:
+			type_size = sizeof( float32_t );
+			break;
+
+		default:
+			// No actions...
+			break;
+	}
+
+	return type_size;
+}
+
+
+
+static par_status_t par_set_u8(const par_name_t par_name, uint8_t u8_val)
+{
+	par_status_t status = par_set_check_validy( par_name );
+
+	if ( ePAR_OK == status )
+	{
+		if ( u8_val > ( gp_par_table[ par_name ].max.u8 ))
+		{
+			*(uint8_t*)&gpu8_par_value[ gu32_par_addr_offset[par_name] ] = gp_par_table[ par_name ].max.u8;
+			status = ePAR_WAR_LIM_TO_MAX;
+		}
+		else if ( u8_val < ( gp_par_table[ par_name ].min.u8 ))
+		{
+			*(uint8_t*)&gpu8_par_value[ gu32_par_addr_offset[par_name] ] = gp_par_table[ par_name ].min.u8;
+			status = ePAR_WAR_LIM_TO_MIN;
+		}
+		else
+		{
+			*(uint8_t*)&gpu8_par_value[ gu32_par_addr_offset[par_name] ] = (uint8_t) ( u8_val );
+			status = ePAR_OK;
+		}
+	}
+
+	return status;
+}
+
+
+static par_status_t par_set_i8(const par_name_t par_name, int8_t i8_val)
+{
+	par_status_t status = par_set_check_validy( par_name );
+
+	if ( ePAR_OK == status )
+	{
+		if ( i8_val > ( gp_par_table[ par_name ].max.i8 ))
+		{
+			*(int8_t*)&gpu8_par_value[ gu32_par_addr_offset[par_name] ] = gp_par_table[ par_name ].max.i8;
+			status = ePAR_WAR_LIM_TO_MAX;
+		}
+		else if ( i8_val < ( gp_par_table[ par_name ].min.i8 ))
+		{
+			*(int8_t*)&gpu8_par_value[ gu32_par_addr_offset[par_name] ] = gp_par_table[ par_name ].min.i8;
+			status = ePAR_WAR_LIM_TO_MIN;
+		}
+		else
+		{
+			*(int8_t*)&gpu8_par_value[ gu32_par_addr_offset[par_name] ] = (int8_t) ( i8_val );
+			status = ePAR_OK;
+		}
+	}
+
+	return status;
+}
+
+/*
+static par_status_t par_set_u16(const par_name_t par_name, uint16_t u16_val)
+{
+	par_status_t status = par_set_check_validy(id);
+
+	if ( ePAR_OK == status )
+	{
+		if ( u16_val > ( par[id].max.u16 ))
+		{
+			par[id].val.u16 = par[id].max.u16;
+			status = ePAR_WAR_LIM_TO_MAX;
+		}
+		else if ( u16_val < ( par[id].min.u16 ))
+		{
+			par[id].val.u16 = par[id].min.u16;
+			status = ePAR_WAR_LIM_TO_MIN;
+		}
+		else
+		{
+			par[id].val.u16 = (uint16_t) ( u16_val );
+			status = ePAR_OK;
+		}
+	}
+
+	return status;
+}
+
+
+static par_status_t par_set_i16(const par_name_t par_name, int16_t i16_val)
+{
+	par_status_t status = par_set_check_validy(id);
+
+	if ( ePAR_OK == status )
+	{
+		if ( i16_val > ( par[id].max.i16 ))
+		{
+			par[id].val.i16 = par[id].max.i16;
+			status = ePAR_WAR_LIM_TO_MAX;
+		}
+		else if ( i16_val < ( par[id].min.i16 ))
+		{
+			par[id].val.i16 = par[id].min.i16;
+			status = ePAR_WAR_LIM_TO_MIN;
+		}
+		else
+		{
+			par[id].val.i16 = (int16_t) ( i16_val );
+			status = ePAR_OK;
+		}
+	}
+
+	return status;
+}
+
+
+static par_status_t par_set_u32(const par_name_t par_name, uint32_t u32_val)
+{
+	par_status_t status = par_set_check_validy(id);
+
+	if ( ePAR_OK == status )
+	{
+		if ( u32_val > ( par[id].max.u32 ))
+		{
+			par[id].val.u32 = par[id].max.u32;
+			status = ePAR_WAR_LIM_TO_MAX;
+		}
+		else if ( u32_val < ( par[id].min.u32 ))
+		{
+			par[id].val.u32 = par[id].min.u32;
+			status = ePAR_WAR_LIM_TO_MIN;
+		}
+		else
+		{
+			par[id].val.u32 = (uint32_t) ( u32_val );
+			status = ePAR_OK;
+		}
+	}
+
+	return status;
+}
+
+
+static par_status_t par_set_i32(const par_name_t par_name, int32_t i32_val)
+{
+	par_status_t status = par_set_check_validy(id);
+
+	if ( ePAR_OK == status )
+	{
+		if ( i32_val > ( par[id].max.i32 ))
+		{
+			par[id].val.i32 = par[id].max.i32;
+			status = ePAR_WAR_LIM_TO_MAX;
+		}
+		else if ( i32_val < ( par[id].min.i32 ))
+		{
+			par[id].val.i32 = par[id].min.i32;
+			status = ePAR_WAR_LIM_TO_MIN;
+		}
+		else
+		{
+			par[id].val.i32 = (int32_t) ( i32_val );
+			status = ePAR_OK;
+		}
+	}
+
+	return status;
+}
+
+
+static par_status_t par_set_f32(const par_name_t par_name, float32_t f32_val)
+{
+	par_status_t status = par_set_check_validy(id);
+
+	if ( ePAR_OK == status )
+	{
+		if ( f32_val > ( par[id].max.f32 ))
+		{
+			par[id].val.f32 = par[id].max.f32;
+			status = ePAR_WAR_LIM_TO_MAX;
+		}
+		else if ( f32_val < ( par[id].min.f32 ))
+		{
+			par[id].val.f32 = par[id].min.f32;
+			status = ePAR_WAR_LIM_TO_MIN;
+		}
+		else
+		{
+			par[id].val.f32 = (float32_t) ( f32_val );
+			status = ePAR_OK;
+		}
+	}
+
+	return status;
+}*/
+
+
+//////////////////////////////////////////////////////////////
+//
+//			par_set_check_validy
+//
+//	param: 		id - parameter id
+//	return:		status
+//	brief:		Check if setting parameter is legal
+//
+//////////////////////////////////////////////////////////////
+static par_status_t par_set_check_validy(const par_name_t par_name)
+{
+	par_status_t status = ePAR_OK;
+
+	// Check if par listed
+	if ( par_name < ePAR_NUM_OF )
+	{
+		// Check access
+		if ( ePAR_ACCESS_RW == ( gp_par_table[ par_name ].access ))
+		{
+			status = ePAR_OK;
+		}
+		else
+		{
+			status = ePAR_ERROR;
+		}
+	}
+	else
+	{
+		status = ePAR_ERROR;
+	}
+
+	return status;
+}
+
+
+
 
 
 
