e8d229ba44d208e51b1ba97bbae3740297019a12
ziga_miklosic <ziga.miklosic@gmail.com>
2021-06-03 14:31:33 +0200

    - implementation of parameter nvm storage

--- src/par_nvm.c
+++ src/par_nvm.c
@@ -26,18 +26,53 @@
 * 			Parameter storage is reserved in "Parameters" region of NVM. Look
 * 			at the nvm_cfg.h/c module for NVM region descriptions.
 *
+*			Parameters stored into NVM in little endianness format.
+*
 * 			Parameter NVM region is further organized into following sections:
 *
 *			-----------------------------------------------------------------
 * 				1. Signature:
 * 			-----------------------------------------------------------------
 *
-* 			Signature tells if NVM "Parameters" region is already initialized
-* 			and has some stored parameter NVM objects.
+* 			Signature tells if NVM "Parameters" region is already initialized.
+*
+*				SIGNATURE DEFINITION:
+*
+*			size: 			4 bytes
+*			base address*:	0x00
+*
+* 						| 	byte 3	  | 	byte 2	  | 	byte 1	 | 		byte 0	  |
+* 						---------------------------------------------------------------
+*						|	0x55	  |		0xAA	  |		0x00     | 		0xFF	  |
+*						---------------------------------------------------------------
+*			address
+*			offset*:		0x03			0x02			0x01			0x00
 *
 *
 *			-----------------------------------------------------------------
-* 				2. Table ID:
+* 				2. Header:
+* 			-----------------------------------------------------------------
+*
+* 			HEADER DEFINITION:
+*
+* 			size:			4 bytes
+* 			base address*:	0x04
+*
+*						| 	byte 7	  | 	byte 6	  | 	byte 5	 | 		byte 4	  |
+* 						---------------------------------------------------------------
+*						|par_num[15:8]| par_num[7:0]  |	  CRC[15:8]  | 		CRC[7:0]  |
+*						---------------------------------------------------------------
+*			address
+*			offset*:		0x07			0x06			0x05			0x04
+*
+*				where: 	par_num - is number of stored parameters
+*						CRC		- checksum of "par_num" value
+*
+*			*relative to "Parameters" NVM region
+*
+*
+*			-----------------------------------------------------------------
+* 				3. Table ID:
 * 			-----------------------------------------------------------------
 *
 *			Table ID is based on hash number calculated based on whole
@@ -48,14 +83,43 @@
 *			parameter table are different. Knowing that copy parameter value
 *			from NVM to RAM is prohibited as data will be misinterpreted.
 *
+*				TABLE ID:
+*
+*			size: 				32 bytes
+*			base address*: 		0x10
+*
+*			*relative to "Parameters" NVM region
 *
 *			-----------------------------------------------------------------
-*				3. Parameters NVM objects
+*				4. Parameters NVM objects
 *			-----------------------------------------------------------------
 *
 *			NVM object consist of parameter value and its 32-CRC checksum. Every
 *			time parameter is being read data integrity is being check based
 *			on it's checksum.
+*
+*			size: 			8 bytes
+*			base address*:	0x100
+*
+* 						| 	byte 3	  | 	byte 2	  | 	byte 1	 | 		byte 0	  |
+* 						---------------------------------------------------------------
+*						|						parameter value						  |
+*						---------------------------------------------------------------
+*			address
+*			offset**:		0x03			0x02			0x01			0x00
+*
+						| 	byte 7	  | 	byte 6	  | 	byte 5	 | 		byte 4	  |
+* 						---------------------------------------------------------------
+*						|						32 - CRC							  |
+*						---------------------------------------------------------------
+*			address
+*			offset**:		0x07			0x06			0x05			0x04
+*
+*
+*			*relative to "Parameters" NVM region
+*			**plus: (8 * parameter ID) + base address
+*
+*			-----------------------------------------------------------------
 *
 *
 * @note		RULES OF "PAR_CFG_TABLE_ID_CHECK_EN" SETTINGS:
@@ -80,6 +144,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 #include "par_nvm.h"
 #include "../../par_cfg.h"
+#include "../../par_if.h"
 
 #if ( 1 == PAR_CFG_NVM_EN )
 
@@ -106,7 +171,10 @@
 	////////////////////////////////////////////////////////////////////////////////
 	// Function Prototypes
 	////////////////////////////////////////////////////////////////////////////////
-	static uint32_t par_nvm_calc_crc32(const uint32_t val);
+	static par_status_t		par_nvm_check_signature	(void);
+	static par_status_t		par_nvm_write_signature	(void);
+	static par_status_t 	par_nvm_check_table_id	(void);
+	static uint32_t 		par_nvm_calc_crc32(const uint32_t val);
 
 	////////////////////////////////////////////////////////////////////////////////
 	// Functions
@@ -114,13 +182,26 @@
 
 	par_status_t par_nvm_init(void)
 	{
-		par_status_t status = ePAR_OK;
+		par_status_t 	status 		= ePAR_OK;
+		par_cfg_t *		p_par_table = NULL;
 
 		// TODO: Check if NVM is initialized
 
-		// TODO: Check for parameter signature
+		// Check NVM signature
+		if ( ePAR_OK == par_nvm_check_signature())
+		{
+			// Rewrite everythink: signature, table ID,...
+			// NOTE: Signature must be written last!!! As this will reduces problems with interrupt of power supply in between NVM write operation!
+		}
+
+		// No signature
+		else
+		{
+			par_nvm_write_signature();
+		}
 
 		// TODO: Calculate and compare table ID
+
 
 		// TODO: Read & set individual paramter from NVM if signature is OK and table ID is the same
 
@@ -151,15 +232,34 @@
 		return status;
 	}
 
+	////////////////////////////////////////////////////////////////////////////////
+	/**
+	* @} <!-- END GROUP -->
+	*/
+	////////////////////////////////////////////////////////////////////////////////
+
+	////////////////////////////////////////////////////////////////////////////////
+	/**
+	*@addtogroup KERNEL_PAR_NVM_FUNCTIONS
+	* @{ <!-- BEGIN GROUP -->
+	*
+	* 	Kernel functions of device parameters NVM handling
+	*/
+	////////////////////////////////////////////////////////////////////////////////
+
 	static uint32_t par_nvm_calc_crc32(const uint32_t val)
 	{
+		uint32_t crc32 = 0;
 
+
+		return crc32;
 	}
 
-#endif // 1 == PAR_CFG_NVM_EN
 
-////////////////////////////////////////////////////////////////////////////////
-/**
-* @} <!-- END GROUP -->
-*/
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
+	/**
+	* @} <!-- END GROUP -->
+	*/
+	////////////////////////////////////////////////////////////////////////////////
+
+#endif // 1 == PAR_CFG_NVM_EN
